4cd844af6d8b9d13180ba367a3fe7d6138b06e43
hl-eulab <hyperlinker@disroot.org>
2025-12-01 08:51:04 +0100

    desktop

--- lab/tec/dt/deepseek/clock.js
+++
@@ -1,6 +0,0 @@
-function aggiornaOra() {
-    const ora = new Date();
-    document.getElementById('orologio').textContent = ora.toLocaleTimeString('it-IT');
-}
-aggiornaOra();
-setInterval(aggiornaOra, 1000);
