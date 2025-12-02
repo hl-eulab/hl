0d936cce0244672bbeb57987e42a6d24bc325b61
hl-eulab <hyperlinker@disroot.org>
2025-12-02 20:35:25 +0100

    earthcal

--- lab/tec/dt/it/561201/test/epoch_ext.js
+++
@@ -1,8 +0,0 @@
-
-function updateEpoch() {
-    let epoch = Math.floor(Date.now() / 1000);
-    document.getElementById("epoch_ext").textContent = epoch;
-}
-updateEpoch();
-setInterval(updateEpoch, 1000);
-
--- lab/tec/dt/it/clock.js
+++
@@ -1,8 +0,0 @@
-
-function aggiornaOra() {
-const ora = new Date();
-document.getElementById('clock').textContent = ora.toLocaleTimeString('it-IT');
-}
-aggiornaOra();
-setInterval(aggiornaOra, 1000);
-
